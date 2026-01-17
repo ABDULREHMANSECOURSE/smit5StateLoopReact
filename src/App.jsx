d252ccef6120d95c71d79ee02848e857176ca69f
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2026-01-17 19:52:44 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,7 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
   for (let i = 1; i <= 5; i++) {
-    console.log('Rendering App component, iteration:', i)
+    console.log(count)
   }
-  return (<div>
-    <h1>Simple Counter</h1>
-    <p>Count: {count}</p>
-    <button onClick={() => setCount(count + 1)}>Increment</button>
-    <button onClick={() => setCount(count - 1)}>Decrement</button>
-  </div>
-  )
 }
-
 export default App
